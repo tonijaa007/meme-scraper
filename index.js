03f4057f51320d111295d43321ff34b9e0ce09bc
Osman Rahimi <tonijaa007@hotmail.com>
2022-09-09 16:38:35 +0200

    Update-meme-scraper

--- index.js
+++ index.js
@@ -1,53 +1,28 @@
+import * as cheerio from 'cheerio';
 import fetch from 'node-fetch';
-import puppeteer from 'puppeteer';
-import xRay from 'x-ray';
 
-// let url = new URL('https://memegen-link-examples-upleveled.netlify.app/');
+const main = async () => {
+  const response = await fetch(
+    'https://memegen-link-examples-upleveled.netlify.app/',
+  );
+  let body = await response.text();
 
-// fetches HTML Code FROM Url
-let memeURL = fetch('https://memegen-link-examples-upleveled.netlify.app/')
-  .then((response) => response.text())
-  .then((data) => console.log(data));
+  body = String(body);
 
-//converts HTML Code (Object) into a string
-memeURL = String(memeURL);
-console.log(typeof memeURL);
+  console.log(body);
 
-/*memeURL.toString();
+  const $ = cheerio.load(body);
 
-// console.log(memeURL);
+  const images = $('a')
+    .children()
+    .map(function (i, el) {
+      return {
+        image: $(this).find('.src').text(),
+      };
+    });
+  const imageSource = images.toArray();
 
-const result = typeof memeURL === 'string' ? true : null;
-console.log(result);
+  console.log(imageSource);
+};
 
-// const imageFinder = xRay();
-
-// Finds all image sources
-/* imageFinder('https://api.memegen.link/images/', 'img', [
-  {
-    img: '',
-    src: '@src',
-    width: '@width',
-    url: '@url',
-  },
-]);
-
-console.log(imageFinder);
-
-
-for (let i = 0; i < imageFinder.length; i++) {
-  if (imageFinder[i] === )
-}
-
-
-function getPosition(imageFinder) {
-  return imageFinder.startsWith('src');
-}
-*/
-
-// const re = new RegExp('<img src.*/>');
-
-// imageFinder.match(re);
-
-// const finder = imageFinder.match(re);
-// console.log(finder);
+main();
